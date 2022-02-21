1eea256b1172848b5e8319a5c687caf057e1b128
WooJin Park <ak2j38@gmail.com>
2022-02-21 21:31:02 +0900

    feature : 입력 및 출력 클래스 구현
    1. InputView에서 당첨 번호를 입력받아 LottoGame에 전달합니다.
    2. OutputView에서는 당첨정보를 출력합니다.
    
    end

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class InputView {
     private static Scanner sc;
     private static String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static String REQUIRED_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public static int getPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
         return sc.nextInt();
+    }
+
+    public static String getRequiredWinningNumber() {
+        System.out.println(REQUIRED_WINNING_NUMBER_MESSAGE);
+        return sc.nextLine();
     }
 
     public static void init() {
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,17 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
 
     private static final String PURCHASED_COUNT_MESSAGE = "개를 구매했습니다.";
+    private static final String WINNING_STATS = "당첨 통계";
+    private static final String DIVISION_LINE = "---------";
+    private static final String CORRECT_THREE_NUMBER = "3개 일치 (5000원)- %d개";
+    private static final String CORRECT_FOUR_NUMBER = "4개 일치 (50000원)- %d개";
+    private static final String CORRECT_FIVE_NUMBER = "5개 일치 (1500000원)- %d개";
+    private static final String CORRECT_SIX_NUMBER = "6개 일치 (2000000000원)- %d개";
+    private static final String TOTAL_PROFIT_MESSAGE = "총 수익률은 %2f%입니다.";
 
     public static void printPurchaseCount(int lottoSize) {
         System.out.println(lottoSize + PURCHASED_COUNT_MESSAGE);
@@ -10,5 +19,15 @@ public class OutputView {
 
     public static void printLottoPaper(String lottoPaper) {
         System.out.println(lottoPaper);
+    }
+
+    public static void printWinningStats(List<Integer> eachRanks, double profit) {
+        System.out.println(WINNING_STATS);
+        System.out.println(DIVISION_LINE);
+        System.out.printf(CORRECT_THREE_NUMBER + "\r\n", eachRanks.get(0));
+        System.out.printf(CORRECT_FOUR_NUMBER + "\r\n", eachRanks.get(1));
+        System.out.printf(CORRECT_FIVE_NUMBER + "\r\n", eachRanks.get(2));
+        System.out.printf(CORRECT_SIX_NUMBER + "\r\n", eachRanks.get(3));
+        System.out.printf(TOTAL_PROFIT_MESSAGE + "\r\n", profit);
     }
 }
