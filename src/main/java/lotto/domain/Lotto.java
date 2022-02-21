cfd7c9fb32e71bfac73926f9da59834e6a65e0a1
WooJin Park <ak2j38@gmail.com>
2022-02-21 20:05:05 +0900

    feature : Lotto 클래스 구현
    1. LottoStore에서 생성자 호출을 통해 생성되며 자체적으로 6개의 숫자리스트를 갖고있는 자료구조입니다.
    
    end

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,22 +1,24 @@
 package lotto.domain;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
 
-    private final List<Integer> lottos = new ArrayList<>();
+    private static final int PICK_NUMBER_LENGTH = 6;
+    private final List<Integer> lottos;
 
-    private Lotto() {
 
+    public Lotto(List<Integer> numbers) {
+        this.lottos = pickSixNumbers(numbers);
     }
 
-    private List<Integer> generateLottos() {
-        List<Integer> numbers = new ArrayList<>();
+    private List<Integer> pickSixNumbers(List<Integer> numbers) {
+        Collections.shuffle(numbers);
 
-        // 45 ?
-//        for (int i = 0; i < array.length; i++) {
-//             = array[i];
-//        }
-//    }
+        return numbers.stream()
+                .limit(PICK_NUMBER_LENGTH)
+                .collect(Collectors.toList());
+    }
 }
