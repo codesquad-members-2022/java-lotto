ee8ff7be46a3fc87f64f277724941bc677338414
Louie <dhdustnr0134@naver.com>
2022-02-21 12:27:05 +0900

    feat : askHowMuchMoney 메서드 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,10 +1,18 @@
 package view;
 
+import domain.Lotto;
+import domain.LottoMachine;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
     private static final Scanner sc = new Scanner(System.in);
 
+    public static List<Lotto> askHowMuchMoney() {
+        int money = sc.nextInt();
+        return LottoMachine.createLottos(money);
+    }
 
 }
