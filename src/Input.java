750e2b9abcd8aeb893442060857386c5f31b073f
mybloom <baejungeun11@gmail.com>
2022-02-21 11:08:29 +0900

    사용자 입력 클래스 작성 : Input
    한 일
    - getInputMoney() : 사용자에게 돈 입력을 받는 메서드
    
    TodoList
    - 로또 게임 설계

--- src/Input.java
+++ src/Input.java
@@ -1,3 +1,17 @@
-public class Input {
+import java.util.Scanner;
 
+public class Input {
+    private static Scanner scanner = new Scanner(System.in);
+
+    private Input() {
+    }
+
+    public static int getInputMoney(String message){
+        System.out.println(message);
+        int money = scanner.nextInt();
+        // TODO 나중에 없애기
+        scanner.nextLine();
+
+        return money;
+    }
 }
