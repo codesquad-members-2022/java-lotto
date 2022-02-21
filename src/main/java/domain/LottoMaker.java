f5ad5c0b54acd211b4fb34b7a2a71a4516d839fe
who-hoo <who.ho3ov@gmail.com>
2022-02-21 16:37:52 +0900

    :sparkles: 랜덤한 번호로 로또를 생성해주는 LottoMaker 클래스 작성

---
+++ src/main/java/domain/LottoMaker.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoMaker {
+
+    private static final List<Integer> allNumbers;
+
+    static {
+        allNumbers = IntStream.rangeClosed(1, 45)
+            .boxed()
+            .collect(Collectors.toList());
+    }
+
+    private LottoMaker() {
+    }
+
+    public static Lotto make() {
+        Collections.shuffle(allNumbers);
+        List<Integer> randomNumbers = allNumbers.subList(0, 6);
+        return new Lotto(randomNumbers);
+    }
+}
