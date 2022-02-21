fd3c27aac69bd846d17606564ea337ab790db356
WooJin Park <ak2j38@gmail.com>
2022-02-21 21:32:47 +0900

    feature : Lotto 클래스 기능 추가
    1. 6개 선택한 번호 리스트 정렬 기능 추가
    2. 당첨 번호와 매칭되는 번호 갯수 반환하는 메소드 추가
    3. 번호 리스트를 문자열로 반환하는 메소드 추가
    
    end

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,11 +7,10 @@ import java.util.stream.Collectors;
 public class Lotto {
 
     private static final int PICK_NUMBER_LENGTH = 6;
-    private final List<Integer> lottos;
-
+    private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        this.lottos = pickSixNumbers(numbers);
+        this.numbers = pickSixNumbers(numbers);
     }
 
     private List<Integer> pickSixNumbers(List<Integer> numbers) {
@@ -19,6 +18,35 @@ public class Lotto {
 
         return numbers.stream()
                 .limit(PICK_NUMBER_LENGTH)
+                .sorted()
                 .collect(Collectors.toList());
+    }
+
+    public String showLottoNumbers() {
+        return numbers.toString();
+    }
+
+    public int getCorrectNumberCount(List<Integer> winningNumbers) {
+        int correctCount = 0;
+
+        for (int number : winningNumbers) {
+             correctCount += correctNumber(number);
+        }
+
+        return correctCount;
+    }
+
+    /**
+     * 현재 숫자가 리스트에 있는지 판단하는 메소드
+     * number가 현재 numbers에 있으면 1 리턴
+     *                       없으면 0 리턴
+     * @param number
+     * @return
+     */
+    private int correctNumber(int number) {
+        if (numbers.contains(number)) {
+            return 1;
+        }
+        return 0;
     }
 }
