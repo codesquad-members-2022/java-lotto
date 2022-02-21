628f7f42f57ddf1e3355f9e9323ad83aafc2ceac
donggi-lee-bit <devdonggilee@gmail.com>
2022-02-21 12:25:20 +0900

    구입금액만큼 로또를 생성하는 기능 추가
    - 섞은 번호를 6개까지 List에 저장하여 반환하였습니다.
    - 구입금액만큼 getShuffledNumbers() 메서드를 실행하여 구입한 개수만큼
      반환하였습니다.

--- src/main/java/app/lotto/Main.java
+++ src/main/java/app/lotto/Main.java
@@ -2,11 +2,51 @@ package app.lotto;
 
 import app.lotto.view.InputView;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
 
         InputView inputView = new InputView();
         int amount = inputView.readAmount();
+        int lottoCount = getLottoCount(amount);
+        getAllShuffledNumbers(lottoCount);
+
+    }
+
+    private static int getLottoCount(int amount) {
+        return amount / 1000;
+    }
+
+    private static List<Integer> getShuffledNumbers() {
+        List<Integer> lottoNumbers = IntStream.rangeClosed(1,45)
+                .boxed()
+                .collect(Collectors.toList());
+
+        Collections.shuffle(lottoNumbers);
+
+        List<Integer> shuffledNumbers = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++) {
+            shuffledNumbers.add(lottoNumbers.get(i));
+        }
+
+        return shuffledNumbers;
+    }
+
+    private static List<List<Integer>> getAllShuffledNumbers(int lottoCount) {
+        List<List<Integer>> allShuffledNumbers = new ArrayList<>();
+        System.out.printf("%d개를 구매했습니다.\n", lottoCount);
+
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> shuffledNumbers = getShuffledNumbers();
+            allShuffledNumbers.add(shuffledNumbers);
+            System.out.println(shuffledNumbers);
+        }
+
+        return allShuffledNumbers;
     }
 }
