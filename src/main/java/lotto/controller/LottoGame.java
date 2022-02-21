1ad53e859ca3db790da5b44355aa7cb931c52c55
WooJin Park <ak2j38@gmail.com>
2022-02-21 21:35:53 +0900

    feature : LottoGame 클래스 기능 추가
    1. 로또 구입을 위해 LottoStore객체에 구입메시지 전송
    2. 로또 각 번호 출력을 위해 OutputView 호출
    3. 당첨 번호 입력받기 위해 InputView 호출
    
    end

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,12 +1,67 @@
 package lotto.controller;
 
+import lotto.domain.LottoPaper;
+import lotto.domain.LottoStore;
 import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoGame {
 
+    private static final String NUMBER_DELIMITER = ",";
+
     public void start() {
         InputView.init();
+
         int purchaseAmount = InputView.getPurchaseAmount();
+
+        LottoStore lottoStore = new LottoStore();
+        LottoPaper lottoPaper = lottoStore.purchase(purchaseAmount);
+
+        OutputView.printPurchaseCount(lottoPaper.getLottoSize());
+        OutputView.printLottoPaper(lottoPaper.showLottoPaper());
+
+        String stringWinningNumbers = InputView.getRequiredWinningNumber();
         InputView.close();
+        List<Integer> winningNumbers = getWinningNumbers(stringWinningNumbers);
+
+        List<Integer> correctCounts = lottoPaper.judgeWinning(winningNumbers);
+//        calculateRank(correctCounts);
+        // 수익률 계산
+
+//        OutputView.printWinningStats();
+
     }
+
+    private List<Integer> getWinningNumbers(String stringWinningNumbers) {
+        List<Integer> winningNumbers = new ArrayList<>();
+        String[] eachNumber = stringWinningNumbers.split(NUMBER_DELIMITER);
+
+        for (String number : eachNumber) {
+            winningNumbers.add(Integer.parseInt(number));
+        }
+
+        return winningNumbers;
+    }
+
+    /**
+     * 각 등수별로 몇 개가 당첨되었는지 리턴
+     * 3개부터 6개까지만 카운트
+     * @param correctCounts
+     * @return
+     */
+//    private List<Integer> calculateRank(List<Integer> correctCounts) {
+//
+//    }
+
+    /**
+     * 수익률 계산 공식
+     * ((당첨금액 / 구매금액) * 100) - 100
+     * @return
+     */
+//    private double calculateProfit() {
+//
+//    }
 }
