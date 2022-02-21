0e8411e9b5b849ac75c7642d222e2d9581adf9dd
KTH-96 <taehyun960@gmail.com>
2022-02-21 16:52:00 +0900

    feat : 인풋뷰클래스에서 로또 구입 금액 입력 기능 추가

---
+++ src/lotto_game/view/InputView.java
@@ -0,0 +1,17 @@
+package lotto_game.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private Scanner sc;
+    private final static String PRICE = "구입금액을 입력해 주세요. ";
+
+    public InputView() {
+        this.sc = new Scanner(System.in);
+    }
+
+    public int inputPrice() {
+        System.out.println(PRICE);
+        return sc.nextInt();
+    }
+}
