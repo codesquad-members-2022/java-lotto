2f0828686cb2eb800c7f6634761d5c92686734ed
Jaehong Choe <qnalsfj@gmail.com>
2022-02-21 20:14:50 +0900

    feat: Main클래스 추가
    - Main클래스 초기화.

---
+++ src/nb993/Main.java
@@ -0,0 +1,20 @@
+package nb993;
+
+import nb993.controller.LottoController;
+import nb993.view.PrintView;
+import nb993.view.ScanView;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ScanView scanView = new ScanView(sc);
+        PrintView printView = new PrintView();
+        LottoController lottoController = new LottoController(scanView, printView);
+        lottoController.buyLotto();
+
+        sc.close();
+
+    }
+}
