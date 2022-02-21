06e9f4fdce5f64fe07a35614de71a93c414d4d5c
geombong <shoy1415@naver.com>
2022-02-21 13:00:16 +0900

    feat. Input클래스 구입 금액 입력 메서드 추가

--- javalotto/src/main/java/view/InputView.java
+++ javalotto/src/main/java/view/InputView.java
@@ -1,4 +1,17 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    private InputView() {
+
+    }
+
+    public static int getPurchaseAmount() {
+        OutputView.printEnterPurchaseAmount();
+        return scanner.nextInt();
+    }
 }
