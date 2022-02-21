609116e71155b417e09fd90704c2b7cecb819959
vans <spentmny@nate.com>
2022-02-21 20:29:05 +0900

    feat : 랜덤으로 여섯개의 값을 받는 Set형성

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -1,7 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class Lotto {
 
+    private List<Integer> numbers;
 
 
+    public Lotto(Set<Integer> numbers) {
+        this.numbers = new ArrayList<>(numbers);
+    }
 }
--- src/main/java/model/LottoController.java
+++ src/main/java/model/LottoController.java
@@ -2,22 +2,34 @@ package model;
 
 import view.InputView;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Array;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoController {
     private int price;
     private static final int LOTTO_PRICE = 1000;
     private int count;
     private List<Lotto> list = new ArrayList<>();
+    private List<Integer> range = IntStream.rangeClosed(1, 45).boxed().collect(Collectors.toList());
 
 
     public void buildLotto() {
         price = Integer.parseInt(InputView.requestPrice());
         count = price / LOTTO_PRICE;
 
-        for (int i = 0; i < count; i++) {
-            list.add(new Lotto());
+        while (true) {
+            Set<Integer> numbers = new HashSet<>();
+            while (numbers.size() != 6) {
+                Collections.shuffle(range);
+                numbers.add(range.get(0));
+            }
+            ArrayList<Integer> lists = new ArrayList<>(numbers);
+            list.add(new Lotto(numbers));
         }
+
     }
+
 }
+
