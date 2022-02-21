0647c219ef7786641f21c67a47455626b0c49ef4
sally-ksh <sally.devz@gmail.com>
2022-02-21 20:55:48 +0900

    feat: 구입금액 입출력
    - 구입금액 입력
    - 구입금액에 따른 로또 구입 개수 출력

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import static views.Input.*;
+import static views.Output.*;
+
+
 public class Main {
 	public static void main(String[] args) {
+		println.accept("구입금액을 입력해 주세요.");
+		int purchaseAmount = nextInt();
 
+		int ticketAccount = purchaseAmount / 1000;
+		println.accept(ticketAccount+"개를 구매했습니다.");
+
+		scanClose();
 	}
+
 }
