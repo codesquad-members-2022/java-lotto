1e4bac9b3907515e8824a4ae2a4980afc5fc58b0
geombong <shoy1415@naver.com>
2022-02-21 13:01:41 +0900

    feat. App클래스 입력 금액별 로또 티켓 구매개수 계산 로직 추가

--- javalotto/src/main/java/application/App.java
+++ javalotto/src/main/java/application/App.java
@@ -1,7 +1,12 @@
 package application;
 
+import view.InputView;
+import view.OutputView;
+
 public class App {
     public static void main(String[] args) {
-
+        int userPurchaseAmount = InputView.getPurchaseAmount();
+        int userPurchaseQuantity = userPurchaseAmount / 1000;
+        OutputView.printPurchaseQuantity(userPurchaseQuantity);
     }
 }
