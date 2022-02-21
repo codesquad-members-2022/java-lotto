41e2f9e06dcb5c07264f44de768fc36d8e74eb5b
PhilSoGooood <peelhw@gmail.com>
2022-02-21 12:17:54 +0900

    기능구현: 로또 구입금액과 당첨번호 입력 기능 구현
    getUserAmount(): 구입금액을 입력받아 정수형으로 바꿔 반환합니다.
    getWinningNumbers(): 당첨번호를 입력받은뒤','를 기준으로 정수형 배열로 바꿔 반환합니다.

---
+++ src/view/InputView.java
@@ -0,0 +1,25 @@
+package view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+	private static final String REQUEST_AMOUNT_MESSAGE = "구입 금액을 입력해 주세요.";
+	private static final String REQUEST_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public int getUserAmount () {
+		System.out.println(REQUEST_AMOUNT_MESSAGE);
+		return Integer.parseInt(scanner.nextLine());
+	}
+
+	public List<Integer> getWinningNumbers() {
+		System.out.println(REQUEST_WINNING_NUMBERS_MESSAGE);
+		String[] winningNumbers = scanner.nextLine().replaceAll(" ", "").split(",");
+		return Arrays.stream(winningNumbers).map(Integer::parseInt).collect(Collectors.toList());
+	}
+
+
+}
