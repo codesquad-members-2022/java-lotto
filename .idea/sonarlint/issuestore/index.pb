946ead9699631622c34ed914e459cfd8ab8a6083
geombong <shoy1415@naver.com>
2022-02-21 23:15:11 +0900

    feat. 당첨통계 출력 로직 추가

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,5 @@
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 A
 javalotto/gradlew,a/9/a99ccc8fc7e766a75d84bf9b2a48f501fb29f491
+F
+javalotto/build.gradle,3/3/336d664c3da0945ff2f222482a4b492445172729
--- javalotto/src/main/java/application/view/OutputView.java
+++ javalotto/src/main/java/application/view/OutputView.java
@@ -2,11 +2,15 @@ package application.view;
 
 import java.util.List;
 
+import application.domain.WinningData;
+
 public class OutputView {
 
     private static final String ENTER_PURCHASE_AMOUNT = "구입 금액을 입력해주세요.";
     private static final String PLEASE_ENTER_YOUR_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String PURCHASE_QUANTITY = "개를 구매하셨습니다.";
+    private static final String WINNING_STATISTICS = "당첨통계";
+    private static final String DIVISION = "---------";
 
     private OutputView() {
 
@@ -26,5 +30,13 @@ public class OutputView {
 
     public static void printPleaseEnterYourWinningNumber() {
         System.out.println(PLEASE_ENTER_YOUR_WINNING_NUMBER);
+    }
+
+    public static void printWinningStatistics() {
+        System.out.println(WINNING_STATISTICS);
+        System.out.println(DIVISION);
+        for (int i = 3; i < 7; i++) {
+            System.out.println(i + "개 일치 " + "(" + WinningData.getWinningData(i) + "원)-" + "개");
+        }
     }
 }
