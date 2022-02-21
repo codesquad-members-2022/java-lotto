fe822b8918e9d7ef5c64c00ee6ac00486890ac7f
geombong <shoy1415@naver.com>
2022-02-21 13:07:41 +0900

    feat. Lotto클래스 생성

---
+++ javalotto/src/main/java/model/Lotto.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lotto {
+
+    private List<Integer> createNumbers() {
+        return new ArrayList<>();
+    }
+
+    public List<Integer> getLotto() {
+        return new ArrayList<>();
+    }
+}
