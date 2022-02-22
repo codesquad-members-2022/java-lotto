6202ed60090c2ab4e08a9ce578a8afacd6f510cd
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-02-22 15:20:47 +0900

    Add methods get user input through console
    * getPaidAmount - 해당 문구를 띄우고 int 값을 반환하게 함. LottoBundle을 생성할 때 사용하기 위함.
    * getWinningNumber - 해당 문구를 띄우고 스트링으로 번호를 받아서 int 배열로 리턴함. 당첨 번호를 가지고 있는 Winning Ticket을 생성하기 위함
    * Close - 스캐너 닫기 위한 메소드.

--- src/lotto/view/InputView.java
+++ src/lotto/view/InputView.java
@@ -1,14 +1,25 @@
 package lotto.view;
 
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class InputView {
+    private final Scanner scanner = new Scanner(System.in);
+
     public int getPaidAmount() {
-        return 0;
+        System.out.println("구입 금액을 입력해주십시오.");
+        return Integer.parseInt(scanner.nextLine());
     }
 
     public int[] getWinningNumber() {
-        return new int[]{};
+        System.out.println("당첨 번호를 입력해주십시오.");
+        String winningNumber = scanner.nextLine();
+        return Stream.of(winningNumber.split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 
     public void close() {
+        scanner.close();
     }
 }
