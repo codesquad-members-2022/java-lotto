2389b457fe0fcdb9c377bfcd4065bd7a852753be
Jaehong Choe <qnalsfj@gmail.com>
2022-02-22 16:23:27 +0900

    feat: Rank enum에 보너스번호 유무를 확인할 수 있도록 기능 추가.
    - Rank.SECOND와 Rank.THIRD를 구별할 수 있는 식별자로 enum자체에 보너스 번호 유무를
    판별할 수 있는 hasBonus변수를 추가.

--- src/nb993/model/Rank.java
+++ src/nb993/model/Rank.java
@@ -2,17 +2,20 @@ package nb993.model;
 
 public enum Rank {
 
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000);
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    NOTHING(0, false, 0);
 
     private int countOfMatch;
     private int winningMoney;
+    private boolean hasBonus;
 
-    private Rank(int countOfMatch, int winningMoney) {
+    private Rank(int countOfMatch, boolean hasBonus, int winningMoney) {
         this.countOfMatch = countOfMatch;
+        this.hasBonus = hasBonus;
         this.winningMoney = winningMoney;
     }
 
@@ -24,18 +27,29 @@ public enum Rank {
         return winningMoney;
     }
 
-    public static Rank valueOf(int countOfMatch, boolean matchBonus) {
+    public boolean hasBonus() {
+        return hasBonus;
+    }
+
+    public static Rank valueOf(int countOfMatch, boolean hasBonus) {
         Rank[] ranks = values();
         for (Rank rank : ranks) {
             if (countOfMatch == SECOND.countOfMatch) {
-                return matchBonus ? SECOND : THIRD;
+                return hasBonus ? SECOND : THIRD;
             }
 
             if (rank.countOfMatch == countOfMatch) {
                 return rank;
             }
         }
+        return NOTHING;
+    }
 
-        return null;
+    @Override
+    public String toString() {
+        if (this.hasBonus) {
+            return this.countOfMatch + "개 일치, 보너스 볼 일치 (" + this.winningMoney + "원)";
+        }
+        return this.countOfMatch + "개 일치 (" + this.winningMoney + "원)";
     }
 }
