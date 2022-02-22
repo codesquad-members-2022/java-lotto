1662fb46e2bc63436eb8ab2b94e17da78457fb08
jun <devjun1023@gmail.com>
2022-02-22 14:54:17 +0900

    LottoTicket 클래스 추가
    Contents
    - Ticket 클래스 추가
    
    Todo
    - LottoStore 클래스 추가

---
+++ domain/LottoTicket.java
@@ -0,0 +1,21 @@
+package PACKAGE_NAME.domain;
+
+import java.util.List;
+
+public class LottoTicket {
+
+    private List<Integer> numbers;
+    private static final int VALID_COUNT = 6;
+
+    public LottoTicket(List<Integer> numbers) {
+        validateCount(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validateCount(List<Integer> numbers) {
+        if (numbers.size() != VALID_COUNT) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
