ebb2933b6156ea976546546c00b07531ab5837ef
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-02-22 15:54:47 +0900

    Declare methods to initialize LottoTicket

--- src/lotto/domain/LottoTicket.java
+++ src/lotto/domain/LottoTicket.java
@@ -1,12 +1,25 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoTicket {
     public static final int PRICE = 1000;
+    private final List<Integer> lottoNumbers;
 
     public LottoTicket() {
-
+        this.lottoNumbers = generateRandomLottoNumbers();
     }
 
     public LottoTicket(int[] winningNumber) {
+        this.lottoNumbers = parseWinningNumber(winningNumber);
+    }
+
+    private List<Integer> generateRandomLottoNumbers() {
+        return new ArrayList<>();
+    }
+
+    private List<Integer> parseWinningNumber(int[] winningNumber) {
+        return new ArrayList<>();
     }
 }
