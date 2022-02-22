c0046ad19dc5a1b99df2291c7fd3fdb68fc03a3f
Minseok-Choi <cms05041@gmail.com>
2022-02-22 12:07:50 +0900

    feat : matchWinningLotto 메서드 구현 및 ranks 필드 추가

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,10 +1,12 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
     private final int money;
     private List<Lotto> lottos;
+    private List<Rank> ranks = new ArrayList<>();
 
     public User(int money) {
         this.money = money;
@@ -21,6 +23,8 @@ public class User {
     public void matchWinningLotto(Lotto winningLotto) {
         for (Lotto lotto : lottos) {
             int count = lotto.getMatchBallCount(winningLotto);
+            Rank status = Rank.createRank(count);
+            ranks.add(status);
         }
     }
 }
