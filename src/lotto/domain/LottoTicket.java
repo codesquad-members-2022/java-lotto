6c59dc112cdc7b7c3a38340d96aab781d5c37e43
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-02-22 17:03:50 +0900

    Implement methods to create LottoTicket
    * 로또 번호 6개는 불변 리스트에 저장된다.
    * 생성자에 매개변수가 없으면 1부터 45 사이의 랜덤한 숫자 6개를 오름차순으로 정렬하여 리스트에 저장한다.
    * 생성자에 int 배열이 매개변수로 주어지면 3가지 검증을 거친 뒤 오름차순으로 정렬하고 리스트로 변환한다.
      * 검증1: 정수 배열의 길이가 6이다.
      * 검증2: 정수 배열의 최소값~최대값 범위가 1~45 안이다.
      * 검증3: 중복되는 수가 없다.

--- src/lotto/domain/LottoTicket.java
+++ src/lotto/domain/LottoTicket.java
@@ -1,25 +1,91 @@
 package lotto.domain;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoTicket {
     public static final int PRICE = 1000;
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 45;
+    private static final int NUMBER_COUNT = 6;
     private final List<Integer> lottoNumbers;
 
     public LottoTicket() {
         this.lottoNumbers = generateRandomLottoNumbers();
     }
 
-    public LottoTicket(int[] winningNumber) {
-        this.lottoNumbers = parseWinningNumber(winningNumber);
+    public LottoTicket(int[] numbers) {
+        validateLength(numbers);
+        validateRange(numbers);
+        validateNoDuplicate(numbers);
+        this.lottoNumbers = parseNumbers(numbers);
+    }
+
+    private void validateRange(int[] numbers) {
+        validateMinimum(numbers);
+        validateMaximum(numbers);
+    }
+
+    private void validateMaximum(int[] numbers) {
+        if (isAboveMaximum(numbers)) {
+            throw new IllegalArgumentException(String.format("로또 번호는 %d보다 작아야 합니다.", MAXIMUM_NUMBER));
+        }
+    }
+
+    private boolean isAboveMaximum(int[] numbers) {
+        return Arrays.stream(numbers).max().orElseThrow() > MAXIMUM_NUMBER;
+    }
+
+    private void validateMinimum(int[] numbers) {
+        if (isBelowMinimum(numbers)) {
+            throw new IllegalArgumentException(String.format("로또 번호는 %d보다 커야 합니다.", MINIMUM_NUMBER));
+        }
+    }
+
+    private boolean isBelowMinimum(int[] numbers) {
+        return Arrays.stream(numbers).min().orElseThrow() < MINIMUM_NUMBER;
+    }
+
+
+    private void validateLength(int[] numbers) {
+        if (numbers.length != NUMBER_COUNT) {
+            throw new IllegalArgumentException(String.format("%d개의 숫자가 필요합니다.", NUMBER_COUNT));
+        }
+    }
+
+    private void validateNoDuplicate(int[] numbers) {
+        if (hasDuplicate(numbers)) {
+            throw new IllegalArgumentException("로또 번호는 각각 달라야 합니다.");
+        }
+    }
+
+    private boolean hasDuplicate(int[] numbers) {
+        int lengthWithoutDuplicate = (int) Arrays.stream(numbers)
+                .distinct()
+                .count();
+        return numbers.length != lengthWithoutDuplicate;
     }
 
     private List<Integer> generateRandomLottoNumbers() {
-        return new ArrayList<>();
+        List<Integer> numbers = IntStream.rangeClosed(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+                .boxed()
+                .collect(Collectors.toList());
+
+        Collections.shuffle(numbers);
+
+        return numbers.stream()
+                .limit(NUMBER_COUNT)
+                .sorted()
+                .collect(Collectors.toUnmodifiableList());
     }
 
-    private List<Integer> parseWinningNumber(int[] winningNumber) {
-        return new ArrayList<>();
+    private List<Integer> parseNumbers(int[] numbers) {
+        return Arrays.stream(numbers)
+                .sorted()
+                .boxed()
+                .collect(Collectors.toUnmodifiableList());
     }
 }
