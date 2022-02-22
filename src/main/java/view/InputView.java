2947b1f4b7b24f7ce824d6cc3154a76d46e509f4
Kyle Choi <tmdfl1027@gmail.com>
2022-02-22 12:09:18 +0900

    [step1] InputView.getAmount 구현
    금액을 받는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,7 +1,13 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private final static Scanner sc = new Scanner(System.in);
+
     public static int getAmount() {
-        return 0;
+        System.out.println("구매 금액을 입력해 주세요.");
+
+        return sc.nextInt();
     }
 }
