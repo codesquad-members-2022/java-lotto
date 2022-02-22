942c5537b5a079090b9a35c0a1ff4f7915c16072
donggi-lee-bit <devdonggilee@gmail.com>
2022-02-22 11:52:00 +0900

    보너스 볼 여부가 포함된 2등을 추가
    - 1등을 제외한 등수를 하나씩 뒤로 미뤘습니다.
    - 보너스 볼 여부를 포함한 2등을 추가하였습니다.

--- src/main/java/app/lotto/domain/LottoPrize.java
+++ src/main/java/app/lotto/domain/LottoPrize.java
@@ -1,17 +1,24 @@
 package app.lotto.domain;
 
 public enum LottoPrize {
-    FOURTH(5_000, 3),
-    THIRD(50_000, 4),
-    SECOND(1_500_000, 5),
-    FIRST(2_000_000_000, 6);
+    FIFTH(5_000, 3, false),
+    FOURTH(50_000, 4, false),
+    THIRD(1_500_000, 5, false),
+    SECOND(30_000_000, 5, true),
+    FIRST(2_000_000_000, 6, false);
 
     private long prizeMoney;
     private int count;
+    private boolean isBonus;
 
-    LottoPrize(long prizeMoney, int count) {
+    LottoPrize(long prizeMoney, int count, boolean isBonus) {
         this.prizeMoney = prizeMoney;
         this.count = count;
+        this.isBonus = isBonus;
+    }
+
+    public boolean isBonus() {
+        return isBonus;
     }
 
     public long getPrizeMoney() {
