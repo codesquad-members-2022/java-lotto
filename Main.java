2ed9feaddb5df96906044ee0a006039fd5828d45
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-02-23 02:45:12 +0900

    LottoCompany에서 몇개의 로또가 맞았는지 체크
    Contents
    - 로또마다 당첨번호와 맞은 개수를 파악해서 HashMap에 카운트
    
    Todo
    - 수익률 계산메서드 구현

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 package PACKAGE_NAME;
 
+import PACKAGE_NAME.domain.LottoCompany;
 import PACKAGE_NAME.domain.LottoStore;
 import PACKAGE_NAME.domain.LottoTicket;
+import PACKAGE_NAME.domain.LottoTickets;
 
 import java.util.List;
 
@@ -11,7 +13,22 @@ public class Main {
         List<Integer> lottoNumbers = lottoStore.getLottoNumbers();
 
         LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
-        lottoTicket.getNumbers().forEach(System.out::println);
+//        lottoTicket.getNumbers().forEach(System.out::println);
+
+
+        LottoCompany lottoCompany = new LottoCompany();
+
+
+        lottoCompany.notifyAnswer(List.of(3,4,5,6,7,8));
+
+
+        lottoCompany.numberMatch(lottoStore.getLottoTickets(20))
+                .entrySet()
+                .forEach(System.out::println);
+
+
+
+
 
     }
 }
--- domain/LottoCompany.java
+++ domain/LottoCompany.java
@@ -1,21 +1,32 @@
 package PACKAGE_NAME.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 
 public class LottoCompany {
     private static List<Integer> winningNumbers = new ArrayList<>();
+    private static final Function<Integer, Integer> add = x -> x + 1;
 
-    public List<Integer> getWinningNumbers(){
+    public List<Integer> getWinningNumbers() {
         Collections.sort(winningNumbers);
         return winningNumbers;
     }
 
     public void notifyAnswer(List<Integer> inputWinningNumber) {
-        if(winningNumbers.size() != 0){
+        if (winningNumbers.size() != 0) {
             winningNumbers = new ArrayList<>();
         }
         winningNumbers.addAll(inputWinningNumber);
+    }
+
+    public Map<Integer, Integer> numberMatch(List<LottoTicket> lottoTickets) {
+        Map<Integer, Integer> answers = new HashMap<>();
+
+        for (int index = 0; index < lottoTickets.size(); index++) {
+            LottoTicket lottoTicket = lottoTickets.get(index);
+            int count = lottoTicket.countAnswer(winningNumbers);
+            answers.put(count,answers.getOrDefault(count,0)+1);
+        }
+        return answers;
     }
 }
