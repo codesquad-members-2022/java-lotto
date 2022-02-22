f755a78b0144875c9927dcb292a1336f055316d6
ikjo93 <auddlr100@naver.com>
2022-02-22 10:58:29 +0900

    feat : 로또 번호 생성 및 출력 기능 구현
    수정사항
    1. Lotto 클래스 생성 : 로또 번호를 생성하고 이를 담고있는 역할을 합니다.
    2. 이와 관련하여 LottoGame 클래스와 OutputView 클래스를 수정하여 사용자 화면(콘솔)에 구입한 로또 장수만큼 로또 번호를 생성하고 출력하도록 했습니다.

--- LottoGame.java
+++ LottoGame.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGame {
 
     public void run() {
@@ -5,5 +8,17 @@ public class LottoGame {
         int purchaseMoney = ui.inputMoney();
         int lottoCount = purchaseMoney / 1000;
         OutputView.showLottoCount(lottoCount);
+
+        List<Lotto> lottoList = createLottoList(lottoCount);
+        OutputView.showLottoList(lottoList);
+
+    }
+
+    private List<Lotto> createLottoList(int lottoCount) {
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lottoList.add(new Lotto());
+        }
+        return lottoList;
     }
 }
--- OutputView.java
+++ OutputView.java
@@ -1,7 +1,24 @@
+import java.util.List;
 
 public class OutputView {
 
+    private static final int LOTTO_NUMBER_MAX_INDEX = 5;    // 6개 로또 번호별 인덱스: [0 ~ 5]
+
     public static void showLottoCount(int lottoCount) {
         System.out.printf("%d개를 구매했습니다.%n", lottoCount);
+    }
+
+    public static void showLottoList(List<Lotto> lottoList) {
+        for (int i = 0; i < lottoList.size(); i++) {
+            showLottoNumber(lottoList.get(i));
+        }
+    }
+
+    private static void showLottoNumber(Lotto lotto) {
+        System.out.print("[");
+        for (int i = 0; i < LOTTO_NUMBER_MAX_INDEX; i++) {
+            System.out.printf("%d,", lotto.getNumbers().get(i));
+        }
+        System.out.println(lotto.getNumbers().get(LOTTO_NUMBER_MAX_INDEX) + "]");
     }
 }
