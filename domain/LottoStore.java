8b2ae9db0bb40c45a361c0bc765fb38fc243c0c7
jun <devjun1023@gmail.com>
2022-02-22 15:34:40 +0900

    랜덤한 숫자 6개 뽑기
    Contents
    - getLottoNumbers메서드 구현
    
    Todo
    - LottoTicket 개발

--- domain/LottoStore.java
+++ domain/LottoStore.java
@@ -3,32 +3,36 @@ package PACKAGE_NAME.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoStore {
     private static List<Integer> lottoNumbers = new ArrayList<>();
 
 
-    static{
+    static {
         init();
     }
 
     private static void init() {
-        IntStream.rangeClosed(1,45)
+        IntStream.rangeClosed(1, 45)
                 .boxed()
                 .forEach(number -> lottoNumbers.add(number));
     }
 
+    // 섞고 -> 6개를 뽑는 메서드
+    public List<Integer> getLottoNumbers() {
+        shuffle();
+        return getSixNumbers();
+    }
 
-    private void shuffle(){
+    private void shuffle() {
         Collections.shuffle(lottoNumbers);
     }
 
-
-    public LottoStore(List<Integer> lottoNumbers) {
-        this.lottoNumbers = lottoNumbers;
+    private List<Integer> getSixNumbers() {
+        return lottoNumbers.stream()
+                .limit(6)
+                .collect(Collectors.toList());
     }
-
-
-
 }
