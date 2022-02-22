6a841ceef326b900de5bccac003443816966b588
Minseok-Choi <cms05041@gmail.com>
2022-02-22 14:52:58 +0900

    refactor : printResult 입출력예시와 동일하게 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -26,7 +26,9 @@ public class OutputView {
     private static String getLottoString(List<Integer> ballNumbers) {
         StringBuilder sb = new StringBuilder();
         sb.append("[");
-        List<String> stringBallNumbers = ballNumbers.stream().map(String::valueOf).collect(Collectors.toList());
+        List<String> stringBallNumbers = ballNumbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.toList());
         sb.append(String.join(NUMBER_DELIMITER, stringBallNumbers));
         sb.append("]");
         return sb.toString();
@@ -35,14 +37,15 @@ public class OutputView {
     public static void printResult(User user) {
         System.out.println("당첨 통계");
         System.out.println("--------");
-        Rank[] ranks = Rank.values();
+        List<Rank> ranks = Arrays.stream(Rank.values())
+                .filter(rank -> !rank.equals(Rank.FAIL))
+                .sorted(Comparator.comparingInt(Rank::getPrice))
+                .collect(Collectors.toList());
         for (Rank rank : ranks) {
             int count = user.countRank(rank);
-            if(rank.equals(Rank.FAIL)) {
-                continue;
-            }
             System.out.printf("%d개 일치 (%d원) - %d개\n", rank.getMatchCount(), rank.getPrice(), count);
         }
+        System.out.printf("총 수익률은 %.2f%%입니다.", user.calculateRateOfReturn());
     }
 
 
