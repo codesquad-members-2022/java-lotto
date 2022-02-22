d899084a081c63ecdfbe9df8e27b47d2b72594e6
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-02-23 01:30:05 +0900

    당첨번호 제공 클래스 구현
    Contents
    - LottoCompany클래스에서 당첨번호를 제공
    
    Todo
    - 회사에 당첨번호 입력구현

--- domain/LottoCompany.java
+++ domain/LottoCompany.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME.domain;public class LottoCompany {
+package PACKAGE_NAME.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoCompany {
+    private static final List<Integer> winningNumbers = new ArrayList<>();
+
+    public List<Integer> getWinningNumbers(){
+        Collections.sort(winningNumbers);
+        return winningNumbers;
+    }
 }
