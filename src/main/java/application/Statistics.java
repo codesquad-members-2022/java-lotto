0de8ea6d0f9ed11eb2f4f44b39b36eaadb03e481
rxdcxdrnine <rkckr1@naver.com>
2022-02-22 15:08:12 +0900

    feat: LotteryGenerator 내 compareEach 테스트 및 출력 구현
    - LotteryGeneratorTest 내 compareEach 메서드 테스트
    - Statistics 클래스로 당첨 통계량 계산 및 출력 구현

--- src/main/java/application/Statistics.java
+++ src/main/java/application/Statistics.java
@@ -1,6 +1,51 @@
 package application;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Statistics {
 
+    private final Map<Prize, Integer> counts = new LinkedHashMap<>() {{
+        put(Prize.THREE, 0);
+        put(Prize.FOUR, 0);
+        put(Prize.FIVE, 0);
+        put(Prize.SIX, 0);
+    }};
+
+    public Statistics(List<Lottery> lotteries) {
+        for (Lottery lottery : lotteries) {
+            int match = lottery.getMatchCount();
+            if (match < Prize.THREE.matchCount) {
+                continue;
+            }
+            Prize prize = Prize.create(match);
+            counts.put(prize, counts.get(prize) + 1);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        counts.keySet().forEach(key -> {
+            sb.append(key.matchCount).append("개 일치(")
+                .append(key.reward).append("원) - ")
+                .append(counts.get(key)).append("개")
+                .append(System.lineSeparator());
+        });
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        LotteryGenerator lotteryGenerator = new LotteryGenerator();
+
+        lotteryGenerator.playLottery(10000);
+        lotteryGenerator.selectWinLottery(List.of(1, 2, 3, 4, 5, 6));
+        lotteryGenerator.compareEach();
+
+        Statistics statistics = new Statistics(lotteryGenerator.getUserLotteries());
+        System.out.println(statistics);
+    }
 
 }
--- src/test/java/application/LotteryGeneratorTest.java
+++ src/test/java/application/LotteryGeneratorTest.java
@@ -2,6 +2,9 @@ package application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -23,4 +26,43 @@ class LotteryGeneratorTest {
         Lottery lotto = lotteryGenerator.create();
         assertThat(lotto.getNumbers().size()).isEqualTo(6);
     }
+
+    @Test
+    @DisplayName("유저 로또 개수 입력 받아 랜덤으로 생성한다.")
+    void userLotteryCountTest() {
+        lotteryGenerator.playLottery(14);
+        assertThat(lotteryGenerator.getUserLotteries().size()).isEqualTo(14);
+    }
+
+    @Test
+    @DisplayName("당첨 로또 번호를 입력 받아 생성한다.")
+    void winLotterySelectTest() {
+        lotteryGenerator.selectWinLottery(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotteryGenerator.getWinLottery().getNumbers())
+            .containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @ParameterizedTest
+    @DisplayName("당첨 번호를 비교해서 매칭되는 개수를 증가시킨다.")
+    @MethodSource("provideParameters")
+    void matchingTest(List<Integer> numbers, int matchCount) {
+        lotteryGenerator.addUserLottery(numbers);
+        lotteryGenerator.selectWinLottery(List.of(1, 2, 3, 4, 5, 6));
+        lotteryGenerator.compareEach();
+
+        assertThat(lotteryGenerator.getMatchCounts())
+            .isEqualTo(List.of(matchCount));
+    }
+
+    private static Stream<Arguments> provideParameters() {
+        return Stream.of(
+            Arguments.of(List.of(1, 2, 3, 4, 5, 6), 6),
+            Arguments.of(List.of(2, 3, 4, 5, 6, 7), 5),
+            Arguments.of(List.of(3, 4, 5, 6, 7, 8), 4),
+            Arguments.of(List.of(4, 5, 6, 7, 8, 9), 3),
+            Arguments.of(List.of(5, 6, 7, 8, 9, 10), 2),
+            Arguments.of(List.of(6, 7, 8, 9, 10, 11), 1)
+        );
+    }
+
 }
