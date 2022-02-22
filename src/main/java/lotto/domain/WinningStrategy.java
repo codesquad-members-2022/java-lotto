d6c29c5923d3e4b942eecc6aaf690e276eaf8c86
WooJin Park <ak2j38@gmail.com>
2022-02-22 15:07:48 +0900

    feature : 당첨정보 열거형 클래스 추가
    1. 매칭된 숫자에 따라 당첨금액 반환 가능
    
    end

---
+++ src/main/java/lotto/domain/WinningStrategy.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+public enum WinningStrategy {
+    ZERO_MATCHES(0, 0),
+    ONE_MATCHES(1, 0),
+    TWO_MATCHES(2, 0),
+    THREE_MATCHES(3, 5_000),
+    FOUR_MATCHES(4, 50_000),
+    FIVE_MATCHES(5, 1_500_000),
+    SIX_MATCHES(6, 2_000_000_000);
+
+    private int correctNumber;
+    private int winningPrice;
+
+    WinningStrategy(int correctNumber, int winningPrice) {
+        this.correctNumber = correctNumber;
+        this.winningPrice = winningPrice;
+    }
+
+    public int getWinningPrice() {
+        return this.winningPrice;
+    }
+}
