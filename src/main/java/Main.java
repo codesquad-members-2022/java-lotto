36949b702b6fac364e2b0e24988a3df3df7a1cac
bugpigg <bugpigg@gmail.com>
2022-02-22 18:51:29 +0900

    feat : 메인 클래스 추가
    - 애플리케이션 수행을 위한 메인 클래스를 추가하였습니다.

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import controller.Controller;
+
+public class Main {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.run();
+    }
+}
