d4fa6671885de3e435f4c9c4c06419e255e1aff9
bugpigg <bugpigg@gmail.com>
2022-02-22 18:37:17 +0900

    feat : 로또 번호 생성 기능 추가
    - 1~45까지의 수중 랜덤으로 6개의 로또 번호를 뽑는 클래스를 추가하였습니다.
    Co-authored-by: hwicode <khc3796@naver.com>

---
+++ src/main/java/domain/LottoGenerator.java
@@ -0,0 +1,32 @@
+package domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+public class LottoGenerator {
+
+    static final int LOTTO_NUMBER_MIN = 1;
+    static final int LOTTO_NUMBER_MAX = 45;
+    static final int LOTTO_NUMBER_SIZE = 6;
+    private List<Integer> shuffledNumbers = new ArrayList<>();
+
+    public LottoGenerator() {
+        createShuffledNumbers();
+    }
+
+    private void createShuffledNumbers() {
+        for (int i = LOTTO_NUMBER_MIN; i <= LOTTO_NUMBER_MAX; i++) {
+            shuffledNumbers.add(i);
+        }
+    }
+
+    private void shuffle() {
+        Collections.shuffle(shuffledNumbers);
+    }
+
+    public List<Integer> getNumbers() {
+        shuffle();
+        return shuffledNumbers.subList(0, LOTTO_NUMBER_SIZE);
+    }
+}
