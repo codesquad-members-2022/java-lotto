09947072e8e863f9b72a95e6cb3dc0a3444a3235
Minseok-Choi <cms05041@gmail.com>
2022-02-22 14:20:06 +0900

    test : createBallTest() 추가

---
+++ src/test/java/domainTest/BallTest.java
@@ -0,0 +1,21 @@
+package domainTest;
+
+import domain.Ball;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class BallTest {
+
+    @Test
+    @DisplayName("숫자 공은 1부터 45까지만 생성할 수 있다.")
+    void createBallTest() {
+        assertThatNoException().isThrownBy(() -> new Ball(1));
+        assertThatNoException().isThrownBy(() -> new Ball(45));
+        assertThatThrownBy(() -> new Ball(0)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Ball(46)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
