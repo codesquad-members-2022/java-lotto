970fa89ba12885fb73b1d49fc7cdb52549abc028
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-02-22 15:08:41 +0900

    Add methods to initialize LottoBundle
    * 구매금액이 자연수임을 검증하고 구매금액에 따른 로또티켓 수를 판단한다.
    * 로또티켓 수만큼 LottoTicket 객체를 생성하여 배열리스트에 담는다.

--- src/lotto/domain/LottoBundle.java
+++ src/lotto/domain/LottoBundle.java
@@ -1,7 +1,27 @@
 package lotto.domain;
 
-public class LottoBundle {
-    public LottoBundle(int paidAmount) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class LottoBundle {
+    private final int ticketCount;
+
+    private final List<LottoTicket> lottoTickets = new ArrayList<>();
+
+    public LottoBundle(int paidAmount) {
+        validatePositiveInt(paidAmount);
+        this.ticketCount = paidAmount / LottoTicket.PRICE;
+    }
+
+    public void issueLottoTicketsWithRandomNumbers() {
+        for (int i = 0; i < ticketCount; i++){
+            lottoTickets.add(new LottoTicket());
+        }
+    }
+
+    private void validatePositiveInt(int paidAmount) {
+        if (paidAmount <= 0) {
+            throw new IllegalArgumentException("돈의 금액은 양의 정수여야 합니다.");
+        }
     }
 }
--- src/lotto/domain/LottoTicket.java
+++ src/lotto/domain/LottoTicket.java
@@ -1,6 +1,12 @@
 package lotto.domain;
 
 public class LottoTicket {
+    public static final int PRICE = 1000;
+
+    public LottoTicket() {
+
+    }
+
     public LottoTicket(int[] winningNumber) {
     }
 }
