0a457004f62cf6f03f3eed804c02fdd3622cf257
WooJin Park <ak2j38@gmail.com>
2022-02-23 15:55:27 +0900

    feature : 보너스 번호 입력받는 기능 추가
    1. InputView에서 보너스번호 입력받는 부분 추가
    
    end

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
     private static Scanner sc;
     private static String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static String REQUIRED_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static String BONUS_BALL_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
 
     public static int getPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
@@ -17,6 +18,11 @@ public class InputView {
 
     public static String getRequiredWinningNumber() {
         System.out.println(REQUIRED_WINNING_NUMBER_MESSAGE);
+        return sc.nextLine();
+    }
+
+    public static String getBonusBallNumber() {
+        System.out.println(BONUS_BALL_NUMBER_MESSAGE);
         return sc.nextLine();
     }
 
