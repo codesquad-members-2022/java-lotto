eff7b907954e805d7beed0be62b206860e7a4704
jiwoo <zbqmgldjfh@gmail.com>
2022-02-23 10:53:54 +0900

    test(LottoTicketSellerTest): 불필요한 주석 제거

--- src/test/java/domain/LottoTicketSellerTest.java
+++ src/test/java/domain/LottoTicketSellerTest.java
@@ -6,16 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class LottoTicketSellerTest {
-
-//    @Test
-//    void countNumberOfTicketsTest() {
-//        LottoTicketSeller lottoTicketSeller = new LottoTicketSeller();
-//        int money = 14000;
-//        assertThat(lottoTicketSeller.countNumberOfTickets(money)).isEqualTo(14);
-//        money = 14500;
-//        assertThat(lottoTicketSeller.countNumberOfTickets(money)).isEqualTo(14);
-//    }
-
     @Test
     void exchangeTicketTest() {
         LottoTicketSeller lottoTicketSeller = new LottoTicketSeller();
