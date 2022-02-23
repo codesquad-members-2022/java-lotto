573656177b7568d4e2cd18590c82142d801fa744
PhilSoGooood <peelhw@gmail.com>
2022-02-23 10:58:14 +0900

    리팩토링: LottoService 클래스 분리
    LottoTickets 클래스에서는 로또를 셔플해서 로또 티켓장수만큼 무작위로
    생성하는 역할을 담당합니다.
    
    LottoService 클래스에서는
    1. 로또 구매구입액수만큼 로또 티켓을 발행합니다.
    2. 로또 당첨여부를 확인합니다.
    3. 수익률을 계산합니다.

--- src/model/LottoService.java
+++ src/model/LottoService.java
@@ -1,55 +1,43 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class LottoService {
 	private static final int TICKET_PRICE = 1000;
-	private static final List<Integer> NUMBERS = initLottoNumbers();
 
-	public List<Lotto> publishLottoTickets(int purchaseAmount){
+	public LottoTickets publishLottoTickets(int purchaseAmount) {
 		int ticketCount = calculateAmount(purchaseAmount);
-		List<Lotto> tickets = new ArrayList<>();
-
-		for(int i=0; i<ticketCount; i++){
-			tickets.add(new Lotto(createTicket()));
-		}
-
-		return tickets;
+		return LottoTickets.createLottoTickets(ticketCount);
 	}
 
-	public Map<Rank, Integer> checkResult(List<Lotto> tickets, List<Integer> winningNumbers){
+	public Map<Rank, Integer> checkResult(LottoTickets tickets, List<Integer> winningNumbers, int bonusNumber) {
+		List<Lotto> ticketList = tickets.getTickets();
+
 		Map<Rank, Integer> result = new LinkedHashMap<>() {{
+			put(Rank.FIFTH, 0);
 			put(Rank.FOURTH, 0);
 			put(Rank.THIRD, 0);
 			put(Rank.SECOND, 0);
 			put(Rank.FIRST, 0);
 		}};
 
-		tickets.forEach(lotto -> {
+		for (Lotto lotto : ticketList) {
 			int ticketResult = checkTicket(lotto.getTicket(), winningNumbers);
+			boolean isWinningBonusNumber = checkBonusNumber(lotto.getTicket(), bonusNumber);
+
 			if (isWinning(ticketResult)) {
-				result.put(Rank.checkRank(ticketResult), result.get(ticketResult) + 1);
+				Rank rank = Rank.checkRank(ticketResult, isWinningBonusNumber);
+				result.put(rank, result.get(rank) + 1);
 			}
-		});
+		}
 
 		return result;
 	}
 
 	public double calculateEarningRate(int purchaseAmount, Map<Rank, Integer> result) {
 		return (double)calculateWinningAmount(result) / purchaseAmount * 100 - 100;
-	}
-
-	private int calculateWinningAmount(Map<Rank, Integer> result) {
-		int winningAmount = 0;
-		for (Rank rank : result.keySet()) {
-			winningAmount += result.get(rank) * rank.getWinningAmount();
-		}
-		return  winningAmount;
 	}
 
 	private boolean isWinning(int ticketResult) {
@@ -62,20 +50,20 @@ public class LottoService {
 			.count();
 	}
 
-	private List<Integer> createTicket() {
-		Collections.shuffle(NUMBERS);
-		return NUMBERS.stream().limit(6).collect(Collectors.toList());
+	private boolean checkBonusNumber(List<Integer> ticket, int bonusNumber) {
+		return ticket.contains(bonusNumber);
 	}
 
-	private int calculateAmount(int purchaseAmount){
+	private int calculateWinningAmount(Map<Rank, Integer> result) {
+		int winningAmount = 0;
+		for (Rank rank : result.keySet()) {
+			winningAmount += result.get(rank) * rank.getWinningAmount();
+		}
+		return winningAmount;
+	}
+
+	private int calculateAmount(int purchaseAmount) {
 		return purchaseAmount / TICKET_PRICE;
 	}
 
-	private static List<Integer> initLottoNumbers() {
-		List<Integer> list = new ArrayList<>();
-		for(int i = 1; i<46; i++){
-			list.add(i);
-		}
-		return list;
-	}
 }
---
+++ src/model/LottoTickets.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoTickets {
+	private static final List<Integer> NUMBERS = initLottoNumbers();
+
+	private List<Lotto> tickets;
+
+
+	private LottoTickets(List<Lotto> tickets) {
+		this.tickets = tickets;
+	}
+
+	public List<Lotto> getTickets() {
+		return tickets;
+	}
+
+	public static LottoTickets createLottoTickets(int ticketCount) {
+		List<Lotto> tickets = new ArrayList<>();
+		for(int i=0; i<ticketCount; i++){
+			tickets.add(createTicketAutomatically());
+		}
+		return new LottoTickets(tickets);
+	}
+
+	private static Lotto createTicketAutomatically(){
+		return new Lotto(createTicket());
+	}
+
+	private static List<Integer> createTicket() {
+		Collections.shuffle(NUMBERS);
+		return NUMBERS.stream().limit(6).collect(Collectors.toList());
+	}
+
+	private static List<Integer> initLottoNumbers() {
+		List<Integer> list = new ArrayList<>();
+		for(int i = 1; i<46; i++){
+			list.add(i);
+		}
+		return list;
+	}
+}
