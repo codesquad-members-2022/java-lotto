5321af29518c1f0cb7a7f7f660cd1c8de9c126e5
jiwoo <zbqmgldjfh@gmail.com>
2022-02-23 10:53:24 +0900

    fix(LottoController): 컨트롤러 수정 완료

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -4,6 +4,7 @@ import domain.LottoTicket;
 import domain.LottoTicketSeller;
 import domain.Person;
 import view.InputView;
+import view.OutputView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,14 +14,30 @@ public class LottoController {
     public void run() {
         int userMoney = InputView.requestMoney();
         LottoTicketSeller seller = new LottoTicketSeller();
-        
+
         Person testUser = new Person("testUser", userMoney, seller);
         testUser.buyRandomLottoTicket(userMoney);
+
         showLottoInfo(testUser);
 
-        for (int result : results) {
-            System.out.print(result + ", ");
+        showResult(makeResult(testUser));
+    }
+
+    private int[] makeResult(Person testUser) {
+        String[] userNumbers = InputView.requestLottoNumbers();
+        return testUser.checkLottoTickets(Arrays.stream(userNumbers).mapToInt(Integer::parseInt).toArray());
+    }
+
+    private void showResult(int[] results) {
+        OutputView.readResult(results);
+        OutputView.showResult();
+        OutputView.showStatistics();
+    }
+
+    private void showLottoInfo(Person user) {
+        List<LottoTicket> ticketList = user.getMyLottoTicketList();
+        for (LottoTicket lottoTicket : ticketList) {
+            lottoTicket.showLottoNumbers();
         }
-        System.out.println();
     }
 }
