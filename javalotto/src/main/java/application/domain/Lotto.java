78f292c21a0b459ad09f02da0cd8b34508c9ddd9
geombong <shoy1415@naver.com>
2022-02-23 12:30:52 +0900

    refactor. Lotto 클래스 일급컬렉션으로 변경

--- javalotto/src/main/java/application/domain/Lotto.java
+++ javalotto/src/main/java/application/domain/Lotto.java
@@ -1,18 +1,50 @@
 package application.domain;
 
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
 
+    private static final int AMOUNT_LOTTO_NUMBER = 6;
+    private static final int TWO_LOTTO_LENGTH_SUM = 12;
     private final List<Integer> numbers;
-    
+
     public Lotto(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
+    private void validateSize(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != AMOUNT_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("로또 번호는 6개만 가능합니다.");
+        }
+    }
 
-    public List<Integer> getNumbers() {
-        return numbers;
+    private void validateDuplicate(List<Integer> lottoNumbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(lottoNumbers);
+        if (nonDuplicateNumbers.size() != AMOUNT_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("로또 번호들은 중복될 수 없습니다.");
+        }
+    }
+
+    public boolean isContainBonusBallNumber(int bonusBall) {
+        return numbers.contains(bonusBall);
+    }
+
+    public int getTheNumberOfMatches(List<Integer> userWinningNumber) {
+        Set<Integer> userWinningNumberSet = new HashSet<>(userWinningNumber);
+        Set<Integer> lottoSet = new HashSet<>(numbers);
+        lottoSet.addAll(new HashSet<>(userWinningNumberSet));
+        return TWO_LOTTO_LENGTH_SUM - lottoSet.size();
+    }
+
+
+    public String lottoNumbersToString() {
+        return numbers.stream().map(String::valueOf)
+                .collect(Collectors.joining(", "));
     }
 }
