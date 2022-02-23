c970a09bf71c284e9d71d0d8335d3e5642981b76
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-02-23 12:02:00 +0900

    LottoTickets클래스에 총 당첨금 계산 메서드 구현
    Contents
    - LottoCompany클래스에서 구매한 로또를  등수별로 저장한 answers 해쉬맵을 LottoTickets가 받아서 총 당첨금 계산
    
    Todo
    - 구매한 금액과 당첨금을 비교하는 수익률 메서드 구현

--- domain/LottoTickets.java
+++ domain/LottoTickets.java
@@ -1,6 +1,7 @@
 package PACKAGE_NAME.domain;
 
 import java.util.List;
+import java.util.Map;
 
 public class LottoTickets {
     private final List<LottoTicket> lottoTickets;
@@ -9,7 +10,37 @@ public class LottoTickets {
         this.lottoTickets = lottoTickets;
     }
 
+    public int winningAmount(Map<Integer, Integer> answers) {
+        //수익률 계산 (당첨금 총합 - 로또 구매금액)*100 / 로또 구매금액
+
+        int sum = 0;
+
+        for (int answerCount : answers.keySet()) {
+            sum += getWinningAmount(answerCount) * answers.getOrDefault(answerCount, 0);
+        }
+        return sum;
+    }
+
+    private int getWinningAmount(int answerCount) {
+        switch (answerCount) {
+            case 3:
+                return 5000;
+            case 4:
+                return 50000;
+            case 5:
+                return 1500000;
+            case 6:
+                return 2000000000;
+            default:
+                return 0;
+        }
+
+    }
+
+
     public List<LottoTicket> getLottoTickets() {
         return lottoTickets;
     }
+
+
 }
