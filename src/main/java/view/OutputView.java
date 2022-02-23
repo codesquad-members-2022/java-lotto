97da692a4d4fa6a671bcf4ddd1b692a0a9be62e6
nathan29849 <phs960418@gmail.com>
2022-02-23 10:36:15 +0900

    [step2] 시작

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -7,8 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 public class OutputView {
-    private final int SELECTED_NUMBER = 6;
-
     public static void completePurchase(int numberOfTickets, int change, List<LottoTicket> tickets) {
         System.out.println(numberOfTickets + "개를 구매했습니다.");
         System.out.println("거스름돈은 " + change + "원 입니다.");
