dcab952ce2afb611a068b5789a2c0e6b9c35fa70
PhilSoGooood <peelhw@gmail.com>
2022-02-23 11:15:17 +0900

    기능추가: 클래스 분리와 기능 추가에 따른 수정
    LottoTickets의 분리와 보너스 번호 기능이 추가됨에 따라
    Controller를 수정하였습니다.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,8 +3,8 @@ package controller;
 import java.util.List;
 import java.util.Map;
 
-import model.Lotto;
 import model.LottoService;
+import model.LottoTickets;
 import model.Rank;
 import view.InputView;
 import view.OutputView;
@@ -16,11 +16,13 @@ public class Controller {
 		LottoService lottoService = new LottoService();
 
 		int purchaseAmount = inputView.getPurchaseAmount();
-		List<Lotto> lottoTickets = lottoService.publishLottoTickets(purchaseAmount);
+		LottoTickets lottoTickets = lottoService.publishLottoTickets(purchaseAmount);
 		outputView.printTickets(lottoTickets);
 
+		//당첨 번호 입력
 		List<Integer> winningNumbers = inputView.getWinningNumbers();
-		Map<Rank, Integer> result = lottoService.checkResult(lottoTickets, winningNumbers);
+		int bonusNumber = inputView.inputBonusNumber();
+		Map<Rank, Integer> result = lottoService.checkResult(lottoTickets, winningNumbers, bonusNumber);
 		double earningRate = lottoService.calculateEarningRate(purchaseAmount, result);
 
 		outputView.printResult(result, earningRate);
