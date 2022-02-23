9d2759a4c4799dd113fdf4ef0d7dc98a4fca86f0
Minseok-Choi <cms05041@gmail.com>
2022-02-23 15:45:56 +0900

    style : 매직 넘버 상수로 처리

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -4,11 +4,14 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Lotto {
+
+    private static final int LOTTO_SIZE = 6;
     private static final String LOTTO_BALLS_ERROR = "로또는 총 6개의 숫자를 필요로 합니다.(중복숫자 불가능)";
+
     private final Set<Ball> balls;
 
     public Lotto(Set<Ball> balls) {
-        if(balls.size() != 6) {
+        if(balls.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(LOTTO_BALLS_ERROR);
         }
         this.balls = balls;
