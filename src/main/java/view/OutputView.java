edfc5d618d372c215ea535765cd8d9d514bd417e
seyeong <seyoung7555@naver.com>
2022-02-23 14:25:01 +0900

    feat(OutputView.showResult): 보너스 번호 당첨 내역 출력기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,20 +9,29 @@ public class OutputView {
         results = input;
     }
 
+    private OutputView() {}
+
     public static void showResult() {
         System.out.println("당첨 통계");
         System.out.println("---------");
-        for (int winningCount = 3; winningCount < results.length; winningCount++) {
-            System.out.printf("%d개 일치 (%d원)- %d개%n", winningCount, Rank.getRank(winningCount).getPrize(), results[winningCount]);
+        for (int resultIndex = 3; resultIndex < results.length - 2; resultIndex++) {
+            System.out.printf("%d개 일치 (%d원)- %d개%n", resultIndex, Rank.getRank(resultIndex).getPrize(), results[resultIndex]);
+        }
+        for (int resultIndex = 7; resultIndex >= 6; resultIndex--) {
+            System.out.printf("%d개 일치", Rank.getRank(resultIndex).getWinningCount());
+            if (resultIndex == 7) {
+                System.out.print(", 보너스볼 일치");
+            }
+            System.out.printf(" (%d원) - %d개%n", Rank.getRank(resultIndex).getPrize(), results[resultIndex]);
         }
     }
 
     public static void showStatistics() {
         int count = 0;
         int prizeSum = 0;
-        for (int winningCount = 0; winningCount < results.length; winningCount++) {
-            count += results[winningCount];
-            prizeSum += Rank.getRank(winningCount).getPrize() * results[winningCount];
+        for (int resultIndex = 0; resultIndex < results.length; resultIndex++) {
+            count += results[resultIndex];
+            prizeSum += Rank.getRank(resultIndex).getPrize() * results[resultIndex];
         }
         double ratio = (double) (prizeSum - count * 1000) / (count * 1000) * 100;
         System.out.printf("총 수익률은 %.2f%%입니다.%n", ratio);
