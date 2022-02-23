6258daed996117bea49c4933c992e692d57dd8c8
PhilSoGooood <peelhw@gmail.com>
2022-02-23 11:06:36 +0900

    리팩토링: Rank의 checkRank 메서드가 보너스 번호를 판별할 수 있도록 변경하였습니다.
    보너스 번호가 추가 됨에 따라 숫자 5개가 맞고 보너스 번호까지 맞은 경우
    2등, 그렇지 않은경우 3등을 반환하도록 수정하였습니다.

--- src/model/Rank.java
+++ src/model/Rank.java
@@ -1,13 +1,11 @@
 package model;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 public enum Rank {
 	FIRST(6, 2_000_000_000),
-	SECOND(5, 15_000_000),
-	THIRD(4, 50_000),
-	FOURTH(3, 5_000);
+	SECOND(5, 30_000_000),
+	THIRD(5, 15_000_000),
+	FOURTH(4, 50_000),
+	FIFTH(3, 5_000);
 
 	private int count;
 	private int winningAmount;
@@ -25,10 +23,15 @@ public enum Rank {
 		return winningAmount;
 	}
 
-	public static Rank checkRank(int count){
-		return Arrays.stream(Rank.values())
-			.filter(r -> r.getCount() == count)
-			.collect(Collectors.toList())
-			.get(0);
+	public static Rank checkRank(int count, boolean bonus) {
+
+		for (Rank rank : Rank.values()) {
+			if (count == SECOND.count) {
+				return bonus ? SECOND : THIRD;
+			}
+			if (count == rank.count)
+				return rank;
+		}
+		return null;
 	}
 }
