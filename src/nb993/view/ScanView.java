51f89a4d2f8a43a5d1f3553286e4adf6f88b1b9f
Jaehong Choe <qnalsfj@gmail.com>
2022-02-23 12:12:28 +0900

    feat: 수동 구매 관련 사용자 인풋 로직 추가
    - 수동으로 구매할 개수와 사용자 로또 지정 번호를 입력받을 수 있도록 추가 구현.
    - 기존 getWinningNumber()에 사용되던 6개 숫자 입력로직을 메서드로 분리하여 재사용.

--- src/nb993/view/ScanView.java
+++ src/nb993/view/ScanView.java
@@ -14,22 +14,42 @@ public class ScanView {
 
     public int getPurchaseAmount() {
         System.out.println("구매 금액을 입력해 주세요.");
-        return sc.nextInt();
+        int purchaseAmount = sc.nextInt();
+        sc.nextLine();
+        return purchaseAmount;
     }
 
     public List<Integer> getWinningNumber() {
-        sc.nextLine();
         System.out.println("당첨 번호를 입력해 주세요.");
-        String[] strNumbers = sc.nextLine().split(",");
-        List<Integer> winningNumbers = new ArrayList<>();
-        for (int i = 0; i < strNumbers.length; i++) {
-            winningNumbers.add(Integer.parseInt(strNumbers[i].trim()));
-        }
-        return winningNumbers;
+        return getNumbers();
     }
 
     public int getBonusNumber() {
         System.out.println("보너스 볼을 입력해 주세요.");
-        return sc.nextInt();
+        int bonusNumber = sc.nextInt();
+        sc.nextLine();
+        return bonusNumber;
+    }
+
+    public int getManualPurchaseCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        int manualPurchaseCount = sc.nextInt();
+        sc.nextLine();
+        return manualPurchaseCount;
+    }
+
+    public List<Integer> getNumbers() {
+        String[] strNumbers = sc.nextLine().split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < strNumbers.length; i++) {
+            numbers.add(Integer.parseInt(strNumbers[i].trim()));
+        }
+
+        return numbers;
+    }
+
+    public List<Integer> getManualNumber() {
+        System.out.println("수동으로 구매할 번호를 입력해주세요.");
+        return getNumbers();
     }
 }
