b3cda360fb781eb23d9799aea000657746d0678b
WooJin Park <ak2j38@gmail.com>
2022-02-23 16:00:13 +0900

    feature : 보너스 번호 출력기능 추가
    1. 당첨번호 통계 중 보너스번호 출력 추가
    
    end

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -12,6 +12,7 @@ public class OutputView {
     private static final String CORRECT_THREE_NUMBER = "3개 일치 (5000원) - %d개";
     private static final String CORRECT_FOUR_NUMBER = "4개 일치 (50000원) - %d개";
     private static final String CORRECT_FIVE_NUMBER = "5개 일치 (1500000원) - %d개";
+    private static final String CORRECT_BONUS_NUMBER = "5개 일치, 보너스 볼 일치 (30000000원) - %d개";
     private static final String CORRECT_SIX_NUMBER = "6개 일치 (2000000000원) - %d개";
     private static final String TOTAL_PROFIT_MESSAGE = "총 수익률은 %.2f%%입니다.";
 
@@ -29,6 +30,7 @@ public class OutputView {
         System.out.printf(CORRECT_THREE_NUMBER + System.lineSeparator(), getRank(winningStrategies, WinningStrategy.THREE_MATCHES));
         System.out.printf(CORRECT_FOUR_NUMBER + System.lineSeparator(), getRank(winningStrategies, WinningStrategy.FOUR_MATCHES));
         System.out.printf(CORRECT_FIVE_NUMBER + System.lineSeparator(), getRank(winningStrategies, WinningStrategy.FIVE_MATCHES));
+        System.out.printf(CORRECT_BONUS_NUMBER + System.lineSeparator(), getRank(winningStrategies, WinningStrategy.FIVE_WITH_BONUS_MATCHES));
         System.out.printf(CORRECT_SIX_NUMBER + System.lineSeparator(), getRank(winningStrategies, WinningStrategy.SIX_MATCHES));
         System.out.printf(TOTAL_PROFIT_MESSAGE + System.lineSeparator(), profit);
     }
