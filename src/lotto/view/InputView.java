317a4abd1b7330118244d6f83f60712ed7eeb8c7
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-02-24 20:32:49 +0900

    Implement general input validation in InputView
    * 부호 없는 정수 입력에 대한 검증
    * 부호 없는 정수의 csv 입력에 대한 검증
    * 32비트 int가 표현 가능한지 여부에 대한 검증

--- src/lotto/view/InputView.java
+++ src/lotto/view/InputView.java
@@ -11,26 +11,22 @@ public class InputView {
 
     public int getPaidAmount() {
         System.out.printf("구입 금액을 입력해주십시오. 로또는 한 장에 %,d원입니다.%n", LottoTicket.PRICE);
-        return getInt();
+        return getUnsignedInt();
     }
 
     public int[] getWinningNumber() {
         System.out.println("당첨 번호를 입력해주십시오.");
-        return getIntArray();
+        return getUnsignedIntArray();
     }
 
     public int getBonusNumber() {
         System.out.println("보너스 볼을 입력해주십시오.");
-        return getInt();
-    }
-
-    public void close() {
-        scanner.close();
+        return getUnsignedInt();
     }
 
     public int getManualTicketCount(int maxCount) {
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
-        int input = getInt();
+        int input = getUnsignedInt();
 
         if (input > maxCount) {
             throw new IllegalArgumentException(String.format("최대 %d까지만 입력할 수 있습니다.", maxCount));
@@ -42,18 +38,50 @@ public class InputView {
     public int[][] getManualLottoNumbers(int manualTicketCount) {
         System.out.printf("조상님이 가르쳐준 번호를 입력해 주세요. (%d장)%n", manualTicketCount);
         return IntStream.range(0, manualTicketCount)
-                .mapToObj(i -> getIntArray())
+                .mapToObj(i -> getUnsignedIntArray())
                 .toArray(int[][]::new);
     }
 
-    private int getInt() {
-        return Integer.parseInt(scanner.nextLine());
+    public void close() {
+        scanner.close();
     }
 
-    private int[] getIntArray() {
-        String winningNumber = scanner.nextLine();
-        return Stream.of(winningNumber.split(","))
-                .mapToInt(Integer::parseInt)
+    private int getUnsignedInt() {
+        String input = scanner.nextLine();
+        return parseUnsignedInt(input);
+    }
+
+    private int[] getUnsignedIntArray() {
+        String input = scanner.nextLine();
+        validateUnsignedIntArrayString(input);
+        return Stream.of(input.split(","))
+                .mapToInt(this::parseUnsignedInt)
                 .toArray();
+    }
+
+    private int parseUnsignedInt(String input) {
+        validateUnsignedIntString(input);
+
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("잘못된 수가 입력되었습니다. 32비트 int로 표현 가능한 값을 입력해야 합니다");
+        }
+    }
+
+    private void validateUnsignedIntString(String input) {
+        if (input.matches("\\d+")) {
+            return;
+        }
+
+        throw new IllegalArgumentException("부호 없는 정수를 입력해야 합니다.");
+    }
+
+    private void validateUnsignedIntArrayString(String input) {
+        if (input.matches("\\d+(\\s*,\\s*\\d+)*")) {
+            return;
+        }
+
+        throw new IllegalArgumentException("쉼표(,)로 분리된 부호 없는 정수들을 입력해야 합니다");
     }
 }
