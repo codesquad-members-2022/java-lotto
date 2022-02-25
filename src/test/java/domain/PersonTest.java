1457a4edff61c8b8b23784c23b31c0e6b66743ec
jiwoo <zbqmgldjfh@gmail.com>
2022-02-25 15:47:07 +0900

    test(PersonTest): Person의 당첨 번호 검증로직 수정

--- src/test/java/domain/PersonTest.java
+++ src/test/java/domain/PersonTest.java
@@ -4,63 +4,38 @@ import domain.factory.CustomTicketFactory;
 import domain.factory.RandomTicketFactory;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PersonTest {
-    @Test
-    void buyRandomTest() {
-        // given
-        Person person = new Person(14000, new LottoTicketSeller(new RandomTicketFactory()));
-        Person person2 = new Person(10000, new LottoTicketSeller(new RandomTicketFactory()));
-
-        //when
-        person.buyRandomLottoTicket(10000);
-        person2.buyRandomLottoTicket(4500);
-
-        // then
-        assertThat(person.getBoughtTicketNumber()).isEqualTo(10);
-        assertThat(person.getRestMoney()).isEqualTo(4000);
-
-        assertThat(person2.getBoughtTicketNumber()).isEqualTo(4);
-        assertThat(person2.getRestMoney()).isEqualTo(5500);
-    }
-
-    @Test
-    void buyCustomTest() {
-        // given
-        Person person = new Person(14000, new LottoTicketSeller(new CustomTicketFactory()));
-
-        // when
-        int[] numbers = new int[]{1, 2, 3, 4, 5, 6};
-        person.buyCustomLottoTicket(numbers);
-
-        //then
-        assertThat(person.getBoughtTicketNumber()).isEqualTo(1);
-        assertThat(person.getRestMoney()).isEqualTo(13000);
-    }
 
     @Test
     void matchingNumberTest() {
         // given
-        Person person = new Person(14000, new LottoTicketSeller(new CustomTicketFactory()));
+        Person person = new Person();
+        LottoTicketSeller seller = new LottoTicketSeller(new CustomTicketFactory());
+
+        String[] winningNumbers = new String[]{"1", "2", "3", "4", "5", "6"};
+
+        int[] numbers2 = new int[]{1, 2, 3, 4, 5, 7};
+        ArrayList<LottoNumber> lottoNumbers2 = Arrays.stream(numbers2)
+                .mapToObj(i -> new LottoNumber(i))
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        int[] numbers3 = new int[]{1, 2, 3, 4, 9, 8};
+        ArrayList<LottoNumber> lottoNumbers3 = Arrays.stream(numbers3)
+                .mapToObj(i -> new LottoNumber(i))
+                .collect(Collectors.toCollection(ArrayList::new));
 
         // when
-        String[] winningNumbers = new String[]{"1", "2", "3", "4", "5", "6"};
-        int[] numbers2 = new int[]{1, 2, 3, 4, 5, 7};
-        person.buyCustomLottoTicket(numbers2);
-        int[] numbers3 = new int[]{1, 2, 3, 4, 9, 8};
-        person.buyCustomLottoTicket(numbers3);
+        person.buyCustomLottoTicket(seller.exchangeTicket(lottoNumbers2));
+        person.buyCustomLottoTicket(seller.exchangeTicket(lottoNumbers3));
 
         // then
         assertThat(person.checkLottoTickets(new WinningNumbers(winningNumbers, "45"))).isEqualTo(new int[]{0, 0, 0, 0, 1, 1, 0, 0});
-    }
-
-    @Test
-    void buyFailTest() {
-        Person person = new Person(14000, new LottoTicketSeller(new RandomTicketFactory()));
-        assertThatThrownBy(() -> person.buyRandomLottoTicket(14001)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> person.buyRandomLottoTicket(-1)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> person.buyRandomLottoTicket(999)).isInstanceOf(IllegalArgumentException.class);
     }
 }
