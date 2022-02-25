7ac1b178749f8682c1ddd483cc5e608144b26543
WooJin Park <ak2j38@gmail.com>
2022-02-25 10:41:07 +0900

    feature : 수동 구매를 위한 입력 기능 추가
    1. 수동으로 몇 장 구매할지 입력 받는 기능
    1. 수동 번호 입력 받는 기능
    
    end

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class InputView {
     private static Scanner sc;
-    private static String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static String REQUIRED_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static String BONUS_BALL_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String MANUAL_LOTTO_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String MANUAL_LOTTO_NUMBERS_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
+    private static final String REQUIRED_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_BALL_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
 
 
     public static int getPurchaseAmount() {
@@ -15,6 +17,19 @@ public class InputView {
         sc.nextLine();
 
         return purchaseAmount;
+    }
+
+    public static int getManualLottoCount() {
+        System.out.println(MANUAL_LOTTO_COUNT_MESSAGE);
+        int manualLottoCount = sc.nextInt();
+        sc.nextLine();
+
+        return manualLottoCount;
+    }
+
+    public static String getManualLottoNumbers() {
+        System.out.println(MANUAL_LOTTO_NUMBERS_MESSAGE);
+        return sc.nextLine();
     }
 
     public static String getRequiredWinningNumber() {
