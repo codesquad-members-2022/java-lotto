338298de60fff0343dd7b8ede09de24827e7bfb5
donggi-lee-bit <devdonggilee@gmail.com>
2022-03-02 22:52:27 +0900

    매직넘버 상수로 추출
    - 1부터 45까지의 로또 생성 번호 범위를 상수로 추출하였습니다.
    - 6개의 자동으로 생성된 로또 번호를 가져오기 위한 숫자 범위 또한 상수로 추출하였습니다.

--- src/main/java/app/lotto/domain/LottoAutoMachine.java
+++ src/main/java/app/lotto/domain/LottoAutoMachine.java
@@ -9,6 +9,9 @@ import java.util.stream.IntStream;
 public class LottoAutoMachine {
 
     private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
 
     public static List<LottoTicket> getAllAutoLottoNumbers(int autoLottoCount) {
         List<LottoTicket> allAutoLottoNumbers = new ArrayList<>();
@@ -28,11 +31,11 @@ public class LottoAutoMachine {
     private static LottoTicket getAutoLottoNumbers() {
         List<Integer> lottoNumbers = getLottoNumbers();
         Collections.shuffle(lottoNumbers);
-        return new LottoTicket(lottoNumbers.subList(0, 6));
+        return new LottoTicket(lottoNumbers.subList(0, LOTTO_NUMBER_COUNT));
     }
 
     private static List<Integer> getLottoNumbers() {
-        return IntStream.rangeClosed(1,45)
+        return IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
                 .boxed()
                 .collect(Collectors.toList());
     }
